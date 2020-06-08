6617ca0ddcc15488bb2fc8e2d7fc30abaf706457
root <root@localhost.localdomain>
2020-06-08 01:11:39 -0400

    first commit

--- check-space.sh
+++ check-space.sh
@@ -4,8 +4,8 @@ do
   echo $output
   usep=$(echo $output | awk '{ print $1}' | cut -d'%' -f1  )
   partition=$(echo $output | awk '{ print $2 }' )
-  if [ $usep -ge 90 ]; then
-    echo "Running out of space \"$partition ($usep%)\" on $(hostname) as on $(date)" |
-     mail -s "Alert: Almost out of disk space $usep%" you@somewhere.com
+  if [ $usep -ge 10 ]; then
+    echo "Running out of space \"$partition ($usep%)\" on $(hostname) as on $(date)" 
+   # | mail -s "Alert: Almost out of disk space $usep%" you@somewhere.com
   fi
-ne
+done
