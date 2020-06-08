f1eb80ea5cdef84198199149e99ddc563a7312ee
root <root@localhost.localdomain>
2020-06-08 01:21:20 -0400

    first commit

--- check-space.sh
+++ check-space.sh
@@ -4,11 +4,14 @@ do
   echo $output
   usep=$(echo $output | awk '{ print $1}' | cut -d'%' -f1  )
   partition=$(echo $output | awk '{ print $2 }' )
-  if [ $usep -ge 90 ]; then
+  
+if [ $usep -ge 90 ]; then
     echo "Running out of space \"$partition ($usep%)\" on $(hostname) as on $(date)" 
-   # | mail -s "Alert: Almost out of disk space $usep%" you@somewhere.com
-	else
-	echo "Could not change directory! Aborting." 1>&2
 	exit 1
-  fi
+   # | mail -s "Alert: Almost out of disk space $usep%" you@somewhere.com
+	
+else
+    echo "DIR is not full , continue deploy code .........................."
+
+fi
 done
